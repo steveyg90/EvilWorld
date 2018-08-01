cf025bba32a3778eebe19a6d89df167f831e45b9
steg90 <steve.green@qa.com>
2018-08-01 17:52:37 +0100

    updated window sizes etc

--- desktop/src/com/evilworld/game/desktop/DesktopLauncher.java
+++ desktop/src/com/evilworld/game/desktop/DesktopLauncher.java
@@ -7,6 +7,14 @@ import com.evilworld.game.EvilWorldGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Project Life V1.0";
+		config.width = 1280;//1600;//1280;//1600;//1600;
+		config.height = 800;//900;//800;//900;//900;//900;
+		config.vSyncEnabled = true;
+		config.backgroundFPS =120;
+		config.foregroundFPS = 120;
+		config.fullscreen = !true;
+		config.resizable = false;
 		new LwjglApplication(new EvilWorldGame(), config);
 	}
 }
